1bcd359101daa37846c5738d63a3c64308c3a6ac
rpost-intertech <62145451+rpost-intertech@users.noreply.github.com>
2020-03-23 14:28:56 -0500

    Quiz Component Added
    Added Quiz component with quiz questions from JSON displayed to page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
+import Quiz from './components/Quiz/Quiz'
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz/>
     )
   }
 }
