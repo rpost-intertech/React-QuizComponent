dc11c075e7f22897b8bed635372e10763fa0ef6a
rpost-intertech <62145451+rpost-intertech@users.noreply.github.com>
2020-03-23 14:36:27 -0500

    Accessing Quiz JSON data
    Changed the way of accessing the data from the JSON file

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,9 +15,7 @@ class Quiz extends Component {
         return (
             <div>
                 <div className="QuizQuestion">
-                       {quizData.quiz_questions.map(item => {
-                          return item.instruction_text
-                       })}  
+                      {quizData.quiz_questions[0].instruction_text} 
                 </div>
             </div>
         );
