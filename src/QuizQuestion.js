8fa1e0e0b07728f4df0b0e353fba6c3f63f05596
rpost-intertech <62145451+rpost-intertech@users.noreply.github.com>
2020-03-23 15:36:31 -0500

    Update QuizQuestion.js
    Add error message for incorrect answer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,10 +3,22 @@ import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            incorrectAnswer: false
+        }
+    }
+
     handleClick(buttonText) {
         if(buttonText === this.props.quiz_question.answer){
             this.props.showNextQuestionHandler();
         }
+        this.setState((state) =>{
+            return{
+                incorrectAnswer: !state.incorrectAnswer
+            }
+        });
     }
 
     render() {
@@ -28,6 +40,7 @@ class QuizQuestion extends Component {
                 
               </ul>
             </section>
+            {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right.</p> : null}
           </main>
         );
     }
